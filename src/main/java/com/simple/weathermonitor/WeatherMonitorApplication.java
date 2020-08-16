97ca02226430c9403a88535ce397ab396f37848d
Caio Melo <caio.alb.melo@gmail.com>
2020-08-16 19:16:40 -0300

    Base project

---
+++ src/main/java/com/simple/weathermonitor/WeatherMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.simple.weathermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMonitorApplication.class, args);
+	}
+
+}
