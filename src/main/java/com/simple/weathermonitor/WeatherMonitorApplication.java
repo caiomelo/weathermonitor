7f26ce23c55b44ee80cd0f741ef7a4e032cdcce8
Caio Melo <caio.alb.melo@gmail.com>
2020-08-23 16:20:41 -0300

    Adds and enables feign clients

--- src/main/java/com/simple/weathermonitor/WeatherMonitorApplication.java
+++ src/main/java/com/simple/weathermonitor/WeatherMonitorApplication.java
@@ -2,8 +2,10 @@ package com.simple.weathermonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WeatherMonitorApplication {
 
 	public static void main(String[] args) {
