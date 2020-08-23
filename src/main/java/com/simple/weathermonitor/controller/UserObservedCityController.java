7e656f04ac6009e5f89cb58df4bc93c77527a4ac
Caio Melo <caio.alb.melo@gmail.com>
2020-08-23 17:42:37 -0300

    Adds user observed city controller, service and dao

--- src/main/java/com/simple/weathermonitor/controller/UserObservedCityController.java
+++ src/main/java/com/simple/weathermonitor/controller/UserObservedCityController.java
@@ -1,9 +1,13 @@
 package com.simple.weathermonitor.controller;
 
+import com.simple.weathermonitor.model.UserObservedCity;
+import com.simple.weathermonitor.service.UserObservedCityService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -11,5 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/userobservedcity")
 public class UserObservedCityController {
 
+    private final UserObservedCityService service;
 
+    @GetMapping
+    public List<UserObservedCity> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public UserObservedCity createOrUpdate(@RequestBody @Valid UserObservedCity userObservedCity) {
+        return service.createOrUpdate(userObservedCity);
+    }
 }
---
+++ src/main/java/com/simple/weathermonitor/service/UserObservedCityService.java
@@ -0,0 +1,23 @@
+package com.simple.weathermonitor.service;
+
+import com.simple.weathermonitor.model.UserObservedCity;
+import com.simple.weathermonitor.repository.UserObservedCityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class UserObservedCityService {
+
+    private final UserObservedCityRepository repository;
+
+    public List<UserObservedCity> findAll() {
+        return repository.findAll();
+    }
+
+    public UserObservedCity createOrUpdate(UserObservedCity userObservedCity) {
+        return repository.save(userObservedCity);
+    }
+}
