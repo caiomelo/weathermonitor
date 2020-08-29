52f4c13445c7e067273fee5711b968d8dd3fa3e0
Caio Melo <caio.alb.melo@gmail.com>
2020-08-29 20:33:52 -0300

    Adds user api documentation

--- src/main/java/com/simple/weathermonitor/controller/UserController.java
+++ src/main/java/com/simple/weathermonitor/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.simple.weathermonitor.controller;
 
-import com.simple.weathermonitor.model.User;
 import com.simple.weathermonitor.model.CityTemperatureInfo;
+import com.simple.weathermonitor.model.User;
 import com.simple.weathermonitor.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "User", description = "Operations on users")
 @Slf4j
 @AllArgsConstructor
 @RestController
@@ -18,18 +23,25 @@ public class UserController {
 
     private final UserService service;
 
-    @GetMapping
+    @Operation(summary = "Retrieve all users", description = "Retrieves a list of all existing users")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> findAll() {
         return service.findAll();
     }
 
-    @PostMapping
+    @Operation(summary = "Create or update a user",
+            description = "Creates a new user or updates an existing one if a valid id is given")
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User createOrUpdate(@RequestBody @Valid User user) {
         return service.createOrUpdate(user);
     }
 
-    @GetMapping("{email}/observation")
-    public List<CityTemperatureInfo> getObservedTemperaturesFor(@PathVariable String email) {
+    @Operation(summary = "Retrieve temperature from active observed cities for a user",
+            description = "Retrieves the current temperature for all cities which are in an active observation period " +
+                    "for the user identified by the given email")
+    @GetMapping(value = "{email}/observation", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CityTemperatureInfo> getObservedTemperaturesFor(
+            @Parameter(description = "User email", required = true) @PathVariable String email) {
         return service.getCityObservationsFor(email);
     }
 }
