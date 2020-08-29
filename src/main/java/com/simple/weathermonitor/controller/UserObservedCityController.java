2cfd4b0ecd26edfb1de644386bb52f813b649113
Caio Melo <caio.alb.melo@gmail.com>
2020-08-29 20:41:49 -0300

    Adds observation info documentation

--- src/main/java/com/simple/weathermonitor/controller/UserObservedCityController.java
+++ src/main/java/com/simple/weathermonitor/controller/UserObservedCityController.java
@@ -2,6 +2,8 @@ package com.simple.weathermonitor.controller;
 
 import com.simple.weathermonitor.model.UserObservedCity;
 import com.simple.weathermonitor.service.UserObservedCityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "User observed city", description = "Operations on user's observed city")
 @Slf4j
 @AllArgsConstructor
 @RestController
@@ -17,11 +20,15 @@ public class UserObservedCityController {
 
     private final UserObservedCityService service;
 
+    @Operation(summary = "Retrieve cities observation periods",
+            description = "Retrieves all existing city observation periods for all users")
     @GetMapping
     public List<UserObservedCity> findAll() {
         return service.findAll();
     }
 
+    @Operation(summary = "Add or update city observation period",
+            description = "Adds a city observation period for a user or updates the existing one if a valid id is given")
     @PostMapping
     public UserObservedCity createOrUpdate(@RequestBody @Valid UserObservedCity userObservedCity) {
         return service.createOrUpdate(userObservedCity);
