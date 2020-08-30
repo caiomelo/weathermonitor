fc5cc6745885f84e500b17d0ebffded30c9731d3
Caio Melo <caio.alb.melo@gmail.com>
2020-08-30 08:39:14 -0300

    Adds documentation for city-related endpoints

--- src/main/java/com/simple/weathermonitor/controller/CityController.java
+++ src/main/java/com/simple/weathermonitor/controller/CityController.java
@@ -1,28 +1,37 @@
 package com.simple.weathermonitor.controller;
 
 import com.simple.weathermonitor.model.accuweather.City;
-import com.simple.weathermonitor.service.WeatherService;
+import com.simple.weathermonitor.service.CityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "City information", description = "Operations on cities and related information")
 @Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/city")
-public class WeatherController {
+public class CityController {
 
-    private final WeatherService weatherService;
+    private final CityService weatherService;
 
-    @GetMapping("/search")
-    public List<City> search(@RequestParam String searchText) {
+    @Operation(summary = "Search for a city",
+            description = "Searches for a city using the given text")
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<City> search(@Parameter(description = "Text to search for") @RequestParam String searchText) {
         return weatherService.search(searchText);
     }
 
-    @GetMapping("/{cityKey}")
-    public City getCityInfo(@PathVariable String cityKey) {
+    @Operation(summary = "Retrieve city information",
+            description = "Retrieves information from the city identified by the given key")
+    @GetMapping(value = "/{cityKey}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public City getCityInfo(@Parameter(description = "City identifier key") @PathVariable String cityKey) {
         return weatherService.getCityInfo(cityKey);
     }
 
